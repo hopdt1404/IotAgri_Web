06acaac74f400c2f8e55692e6adedfe98c57f059
hopdt1404 <hopdt1404@gmail.com>
2021-07-12 13:52:46 +0700

    #1 update css, vue

--- resources/js/components/NavbarVertical.vue
+++ resources/js/components/NavbarVertical.vue
@@ -1,18 +1,18 @@
 <template>
   <div v-if="user">
     <nav class="navbar navbar-expand-lg navbar-light bg-white">
-        <ul class="nav flex-column">
+        <ul class="navbar-nav flex-column">
           <li class="nav-item">
-            <router-link :to="{name: 'farm'}">
+            <router-link class="nav-link" active-class="active" :to="{name: 'farm'}">
               <i class="fa fa-first-order" aria-hidden="true">
-                {{ $t('farm')}}
+                {{ $t('farm_nav')}}
               </i>
             </router-link>
           </li>
           <li class="nav-item">
-            <router-link :to="{name: 'device'}">
+            <router-link class="nav-link" active-class="active" :to="{name: 'device'}">
               <i class="fa fa-first-order" aria-hidden="true">
-                {{ $t('device')}}
+                {{ $t('device_nav')}}
               </i>
             </router-link>
           </li>
--- resources/js/layouts/default.vue
+++ resources/js/layouts/default.vue
@@ -2,20 +2,28 @@
   <div class="main-layout">
     <navbar />
 
-    <div class="container mt-4">
-      <child />
+    <div class="mt-4 row">
+      <div class="col-1 custom-grid">
+        <navbar-vertical />
+      </div>
+      <div class="col-11 custom-grid">
+        <child />
+      </div>
+
     </div>
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar'
+import NavbarVertical from '~/components/NavbarVertical'
 
 export default {
   name: 'MainLayout',
 
   components: {
-    Navbar
+    Navbar,
+    NavbarVertical
   }
 }
 </script>
